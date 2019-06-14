ea196044d13a49cfe8a22be1727023947f911ce8
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-15 00:29:03 +0200

    Restructured chapter 8 folder + updated MadLibs.py

---
+++ Chapter 8/Final Practice Projects/MadLibs.py
@@ -0,0 +1,31 @@
+import os,sys
+
+'''
+## The commented code are notes/tests from chapter 8 of the book
+## They may be ignored for now
+
+#print(os.path.join('usr', 'bin', 'spam'))
+
+print('My print: ')
+#print(os.getcwd())
+#os.chdir('C:\\Windows\\System32')
+#print(os.getcwd())
+
+#print(os.path.abspath('.'))
+
+print(os.path.relpath('C:\\Windows', 'C:\\Users\\nicol\\PycharmProjects'))
+os.chdir('C:\\Windows')
+os.chdir('C:\\')
+
+#print(os.getcwd())
+'''
+## TODO: Read file & imput word corresponding to correct word class
+
+if __name__ == '__main__':
+    if ".txt" in sys.argv[1].lower():
+        myFile = open(sys.argv[1],'w')
+        print(sys.argv[1])
+        myFile.close()
+    else:
+        print('Input .txt file in folder as argument to program call')
+    sys.exit()
