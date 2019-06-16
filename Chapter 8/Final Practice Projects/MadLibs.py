b2721d3f657e02b5ad2774abf2a27cecd9d761ba
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-16 23:17:19 +0200

    Further implemented functionality on MadLibs. Still missing to copy back to file and error handling

--- Chapter 8/Final Practice Projects/MadLibs.py
+++ Chapter 8/Final Practice Projects/MadLibs.py
@@ -1,4 +1,4 @@
-import os,sys
+import os,sys,re
 
 '''
 ## The commented code are notes/tests from chapter 8 of the book
@@ -25,13 +25,41 @@ if __name__ == '__main__':
 
     if len(sys.argv) >= 2 and ('.txt' in sys.argv[1]):
         myFile = open(sys.argv[1],'r+')
-        fileContent = myFile.read()
-        print(fileContent)
+        #fileContent = myFile.read()
+        #print(fileContent)
+
+        # Use RegEx to find all words (upper- and lowercase) larger than 4 and smaller than 9 (covers possible NOUN, VERB and ADJECTIVE)
+        #allMatchingWords = re.findall(r'\b[a-åA-Å]{4,9}\b', fileContent)
+        # Could also have split each line by space
+        # for i in lines:
+        #    thisline = i.split(" ");
+        # or this snippet:
+        allWords = map(lambda l: l.split(" "), myFile.readlines())
+        allWords = list(allWords)
+        #print(allWords[0][0])
+        i = 0
         # Go through each word and replace with input
-        for word in fileContent:
-            if word.toUpper() == 'ADJECTIVE' or word.toUpper() == 'NOUN' or word.toUpper() == 'VERB':
-                print('Please enter a word for the '+ word)
-            print(word)
+        for i in allWords:
+            for j in i:
+                print(i)
+                print(j)
+            '''if word.upper() == 'ADJECTIVE' or word.upper() == 'NOUN' or word.upper() == 'VERB':
+                a = 'an' if word.upper() == 'ADJECTIVE' else 'a'  # Simply to print an or a depending on word
+                print('Please enter ' + a + ' ' + word.lower() + ":")
+                # try:
+                allWords = input()
+                # except:'''
+        '''for word in allWords:#allMatchingWords:
+            if word.upper() == 'ADJECTIVE' or word.upper() == 'NOUN' or word.upper() == 'VERB':
+                a = 'an' if word.upper() == 'ADJECTIVE' else 'a' #Simply to print an or a depending on word
+                print('Please enter '+ a +' '+ word.lower()+":")
+                #try:
+                allWords = input()
+                #except:
+            i+=1'''
+
+
+
         myFile.close()
     else:
         print('ERROR: Missing a .txt file as input argument to file')
