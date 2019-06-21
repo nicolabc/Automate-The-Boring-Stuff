f324245cdc979ce2ca7f7d1f947831580970b969
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-22 00:14:52 +0200

    Finished final project selective copy

--- Chapter 9/selective_copy.py
+++ Chapter 9/selective_copy.py
@@ -1,12 +1,14 @@
 #! python3
-import sys,os,shutil
-
+import sys,os,shutil,pathlib
 if __name__ == '__main__':
     # Select which file extension to copy to the new folder
     fileExtension = '.txt'
 
-    mainFolder = 'C:\\Users\\nicol\\PycharmProjects\\AutomateTheBoringStuff\\Automate-The-Boring-Stuff\\Chapter 9'
+    mainFolder = os.getcwd()#'C:\\Users\\nicol\\PycharmProjects\\AutomateTheBoringStuff\\Automate-The-Boring-Stuff\\Chapter 9'
     destinationFolder = mainFolder + '\\selective_copy_folder'
+
+    pathlib.Path(destinationFolder).mkdir(parents=True, exist_ok=True)
+
 
     # Does a three walk of all folders and files in os.walk(PATH)
     for folderName, subfolders, filenames in os.walk(mainFolder):
@@ -17,7 +19,8 @@ if __name__ == '__main__':
         for filename in filenames:
             print('FILE INSIDE ' + folderName + ': ' + filename)
             if filename.endswith(fileExtension):
-                shutil.copy(filename,destinationFolder)
+                path = os.path.join(destinationFolder, filename)
+                shutil.copy(os.path.join(subfolder,filename),path)
 
         print('')
 
