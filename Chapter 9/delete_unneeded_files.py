2d65f03c5e93c8070846085d7c92521dfc1f3d96
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-22 20:56:53 +0200

    Continued on task. First folder files are found, but 2nd level and further down files in folders are not found. Can't get the proper path as of yet

--- Chapter 9/delete_unneeded_files.py
+++ Chapter 9/delete_unneeded_files.py
@@ -1,19 +1,22 @@
 #! python3
-import sys
+import sys,os
 if __name__ == '__main__':
 
+    mainFolder = os.getcwd()
 
     # Does a three walk of all folders and files in os.walk(PATH)
-    for folderName, subfolders, filenames in os.walk(mainFolder):
-        print('The current folder is ' + folderName)
-
+    for folderName, subfolders, filenames in os.walk(os.getcwd()):
+        #print('The current folder is ' + folderName)
+        path = mainFolder
         for subfolder in subfolders:
-            print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+            #print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+            continue
         for filename in filenames:
-            print('FILE INSIDE ' + folderName + ': ' + filename)
-            if filename.endswith(fileExtension):
-                path = os.path.join(destinationFolder, filename)
-                shutil.copy(os.path.join(subfolder,filename),path)
+            #print('FILE INSIDE ' + folderName + ': ' + filename)
+
+            path = os.path.join(path, filename)
+            print(os.path.abspath('selective_copy.py')) #Ikke riktig path...
+            print(os.path.getsize(os.path.abspath(filename)))
 
         print('')
 
