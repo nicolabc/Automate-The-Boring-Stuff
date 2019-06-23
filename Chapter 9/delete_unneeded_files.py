136881778d9368410f681b25f00526b88aa03892
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-23 12:30:32 +0200

    Finished delete_unneeded_files.py

--- Chapter 9/delete_unneeded_files.py
+++ Chapter 9/delete_unneeded_files.py
@@ -2,7 +2,7 @@
 import sys,os
 if __name__ == '__main__':
 
-    mainFolder = os.getcwd()
+    """mainFolder = os.getcwd()
 
     # Does a three walk of all folders and files in os.walk(PATH)
     for folderName, subfolders, filenames in os.walk(os.getcwd()):
@@ -18,6 +18,15 @@ if __name__ == '__main__':
             print(os.path.abspath('selective_copy.py')) #Ikke riktig path...
             print(os.path.getsize(os.path.abspath(filename)))
 
-        print('')
+        print('')"""
+    for root, dirs, files in os.walk("."):
+        for d in dirs:
+            #print(os.path.relpath(os.path.join(root, d), "."))
+            continue
+        for f in files:
+            path = os.path.relpath(os.path.join(root, f), ".")
+            print(f +" has a filesize of "+ str(os.path.getsize(path))+" bytes")
+            print('')
+            # May add a case where we delete files bigger than 100 Mbytes
 
     sys.exit()
