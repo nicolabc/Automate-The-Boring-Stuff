5370aac9be3485aace26a7874bc3f615d49f5ecb
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-25 23:20:08 +0200

    Started on chap 10

---
+++ Chapter 10/chap10testing.py
@@ -0,0 +1,20 @@
+#! python3
+import sys, logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+if __name__ == '__main__':
+    #raise Exception('This is the error message')
+    # podBayDoorStatus = 'open'
+    # assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+    # podBayDoorStatus = 'I\'m sorry, Dave. I\'m afraid I can\'t do that.'
+    # assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+
+    ## Assertion
+
+    # value = 1
+    # assert value == 1,'HAS TO BE 1!'
+    # value += 1
+    # assert value == 1,'HAS TO BE 1!!'
+    # print(value)
+
+
+    sys.exit()
