96ab085a44c2488fc2c684bcb56601d76f99333d
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-06-27 23:13:15 +0200

    Updated project

--- Chapter 10/chap10testing.py
+++ Chapter 10/chap10testing.py
@@ -16,6 +16,11 @@ if __name__ == '__main__':
     # assert value == 1,'HAS TO BE 1!!'
     # print(value)
 
+    #logging.debug()
+    #logging.info()
+    #logging.warning()
+    #logging.error()
+    #logging.critical()
     logging.disable(logging.CRITICAL)
 
     sys.exit()
