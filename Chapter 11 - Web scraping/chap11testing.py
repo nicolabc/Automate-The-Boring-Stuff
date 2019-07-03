cf554346d5127a94ffc5a040da8d8679a6fd00b7
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-07-03 23:26:44 +0200

    Updating folder name

---
+++ Chapter 11 - Web scraping/chap11testing.py
@@ -0,0 +1,7 @@
+import webbrowser,requests
+#webbrowser.open('http://inventwithpython.com/')
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(type(res))
+print(res.status_code == requests.codes.ok)
+print(len(res.text))
+print(res.text[:250])
