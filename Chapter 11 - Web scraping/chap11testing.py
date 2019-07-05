bef3b8643ad394821b3905874563a106923abfb2
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-07-05 23:34:33 +0200

    Updated project

--- Chapter 11 - Web scraping/chap11testing.py
+++ Chapter 11 - Web scraping/chap11testing.py
@@ -5,3 +5,19 @@ print(type(res))
 print(res.status_code == requests.codes.ok)
 print(len(res.text))
 print(res.text[:250])
+res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print('There was a problem: %s' % (exc))
+
+# Always call raise_for_status() after calling requests.get().
+# You want to be sure that the download has actually worked before your program continues.
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+        playFile.write(chunk)
+playFile.close()
+
