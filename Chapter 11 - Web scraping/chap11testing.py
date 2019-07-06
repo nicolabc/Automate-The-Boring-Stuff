3575260a5e303568a714da2c869a1664117ab41c
nicolabc <31611304+nicolabc@users.noreply.github.com>
2019-07-06 17:17:10 +0200

    Updated project

--- Chapter 11 - Web scraping/chap11testing.py
+++ Chapter 11 - Web scraping/chap11testing.py
@@ -1,10 +1,10 @@
-import webbrowser,requests
+import webbrowser,requests, bs4
 #webbrowser.open('http://inventwithpython.com/')
-res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
-print(type(res))
-print(res.status_code == requests.codes.ok)
-print(len(res.text))
-print(res.text[:250])
+#res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+#print(type(res))
+#print(res.status_code == requests.codes.ok)
+#print(len(res.text))
+#print(res.text[:250])
 # res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
 # try:
 #    res.raise_for_status()
@@ -14,10 +14,22 @@ print(res.text[:250])
 # Always call raise_for_status() after calling requests.get().
 # You want to be sure that the download has actually worked before your program continues.
 
-res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+#res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+#res.raise_for_status()
+#playFile = open('RomeoAndJuliet.txt', 'wb')
+#for chunk in res.iter_content(100000):
+#        playFile.write(chunk)
+#playFile.close()
+
+res = requests.get('http://nostarch.com')
 res.raise_for_status()
-playFile = open('RomeoAndJuliet.txt', 'wb')
-for chunk in res.iter_content(100000):
-        playFile.write(chunk)
-playFile.close()
+noStarchSoup = bs4.BeautifulSoup(res.text,"html.parser")
+print(type(noStarchSoup))
+elems = noStarchSoup.select('#author')
+print(type(elems))
+print(len(elems))
+print(type(elems[0]))
+print(elems[0].getText())
+print(str(elems[0]))
+print(elems[0].attrs)
 
