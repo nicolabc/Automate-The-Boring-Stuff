8882d9dbfdb4d195b1be41cc20faf0e532b791a9
Nicolas B. Carbone <nicolasbcarbone@gmail.com>
2019-07-10 23:12:15 +0200

    Update chap11testing.py

--- Chapter 11 - Web scraping/chap11testing.py
+++ Chapter 11 - Web scraping/chap11testing.py
@@ -35,4 +35,4 @@ print(elems[0].attrs)
 
 # BeautifulSoup 4
 # Documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-
+# Note that RegEx is not possible on <html> websites. It does not parse well...
