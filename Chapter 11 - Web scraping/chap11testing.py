593f5c1e55354aff4d3d76d585127bc92d583737
Nicolas B. Carbone <nicolasbcarbone@gmail.com>
2019-07-14 23:35:12 +0200

    Update chap11testing.py

--- Chapter 11 - Web scraping/chap11testing.py
+++ Chapter 11 - Web scraping/chap11testing.py
@@ -37,6 +37,3 @@ print(elems[0].attrs)
 # Documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 # Note that RegEx is not possible on <html> websites. It does not parse well...
 # See this note: https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags
-# Use beautiful soup 4 instead
-# Can use regex for small extractions
-# https://automatetheboringstuff.com/chapter11
